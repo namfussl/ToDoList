59a08b6d17982db6332bf33b92f5cff32e6cca98
namfussl <gerd.hackl@gmail.com>
2017-07-27 15:23:50 +0100

    Initial check-in of module iOSToDoGH

---
+++ iOSToDoGH/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace iOSToDoGH
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://iostodogh.azurewebsites.net";
+	}
+}
+
